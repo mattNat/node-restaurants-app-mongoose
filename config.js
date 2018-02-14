ae5149fb2392514b7c5b0eecd0b588ca89dc2f67
mattNat <mattsanpedro76@gmail.com>
2018-02-14 09:37:23 -0800

    run test please 2

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://admin:admin123@localhost/restaurants-app?authSource=admin';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://admin:admin123@localhost/test-restaurants-app?authSource=admin';
+// exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://admin:admin123@localhost/restaurants-app?authSource=admin';
+// exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://admin:admin123@localhost/test-restaurants-app?authSource=admin';
+// exports.PORT = process.env.PORT || 8080;
+
+exports.DATABASE_URL = 'mongodb://admin:admin123@localhost/restaurants-app?authSource=admin';
+exports.TEST_DATABASE_URL = 'mongodb://admin:admin123@localhost/test-restaurants-app?authSource=admin';
 exports.PORT = process.env.PORT || 8080;
